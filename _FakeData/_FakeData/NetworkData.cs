5ed48025392009e0e544f729b8ed86e438c8de23
Huseyn95 <huseyn-mikayil@mail.ru>
2017-08-30 23:04:32 +0400

    EmailAddress

---
+++ _FakeData/_FakeData/NetworkData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _FakeData
+{
+    public static class NetworkData
+    {
+        private static Random _rnd;
+        private static List<string> _maleMailAddresses = new List<string>();
+
+        private static List<string> PrepareEmail(string domen)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                string mail = NameData.GetMaleFirstName() + "." + NameData.GetMaleSurname() + "@" + domen;
+                _maleMailAddresses.Add(mail);
+            }
+
+            return _maleMailAddresses;
+            
+        }
+
+        private static string Get(List<string> list)
+        {
+            _rnd = new Random();
+            int number = _rnd.Next(list.Count - 1);
+            return list[number];
+        }
+
+        public static string GetMaleMailAddress(string domen)
+        {
+            return Get(PrepareEmail(domen));
+        }
+    }
+}
---
+++ _FakeData/_TestData/Program.cs
@@ -0,0 +1,18 @@
+﻿using _FakeData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _TestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NetworkData.GetMaleMailAddress("box.az"));
+            Console.ReadLine();
+        }
+    }
+}
